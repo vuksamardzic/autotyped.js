ec809a17d391e25dcc9e05a20be784cbdaa1b937
Vuk Samardžić <samardzic.vuk@gmail.com>
2017-12-23 00:10:11 +0100

    demo code cleaned

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,20 +1,10 @@
 window.onload = function (ev)
 {
     autotyped.init({speed: 200});
-
-    document.getElementById('autotyped').addEventListener('click', function (ev)
-    {
-        autotyped.init({selector: '#autotyped', speed: 200, animation: 'rotate'});
-    });
-
     document.querySelector('.type-button').addEventListener('click', function (ev)
     {
         var an = document.querySelector('#animation-select').value;
         var speed = document.querySelector('#speed-select').value;
-
-        console.log(an, speed);
-
         autotyped.init({speed: speed, animation: an});
-        // debugger;
     });
 };
