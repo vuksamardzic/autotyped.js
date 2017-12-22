597599d2a7342a6a0bda27f00e61a3f32346f82f
Vuk Samardžić <samardzic.vuk@gmail.com>
2017-12-22 18:20:38 +0100

    gulp setup and dist updated

--- gulpfile.js
+++ gulpfile.js
@@ -13,18 +13,22 @@ const autoprefixer      = require('autoprefixer');
 const concat            = require('gulp-concat');
 const uglify            = require('gulp-uglify');
 const surge             = require('gulp-surge');
+const htmlmin           = require('gulp-htmlmin');
+const htmlreplace       = require('gulp-html-replace');
+
 
 /**
  * Development setup
  */
 
-gulp.task('serve', ['compile:js' ,'compile:scss'], function()
+gulp.task('serve', ['compile:js', 'compile:customjs', 'compile:scss'], function()
 {
     browser_sync.init({ server: './' });
 
     gulp.watch('./src/scss/**/*.scss', ['compile:scss']);
     gulp.watch('*.html', browser_sync.reload);
-    gulp.watch('./src/js/**/*.js', ['compile:js', browser_sync.reload]);
+    gulp.watch('./src/js/autotyped.js', ['compile:js', browser_sync.reload]);
+    gulp.watch('./src/js/custom.js', ['compile:customjs', browser_sync.reload]);
 });
 
 gulp.task('compile:scss', function()
@@ -39,12 +43,23 @@ gulp.task('compile:scss', function()
 
 gulp.task('compile:js',  function()
 {
-    return gulp.src('./src/js/**/*.js')
+    return gulp.src('./src/js/autotyped.js')
         .on('error', function (error) {
             console.log(error.toString());
             this.emit('end');
         })
         .pipe(concat('autotyped.js'))
+        .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('compile:customjs',  function()
+{
+    return gulp.src('./src/js/custom.js')
+        .on('error', function (error) {
+            console.log(error.toString());
+            this.emit('end');
+        })
+        .pipe(concat('custom.js'))
         .pipe(gulp.dest('./dist/'));
 });
 
@@ -63,14 +78,59 @@ gulp.task('minify:css', function()
 
 gulp.task('minify:js',  function()
 {
-    return gulp.src('./src/js/**/*.js')
+    return gulp.src('./src/js/autotyped.js')
         .pipe(concat('autotyped.min.js'))
         .pipe(uglify())
         .pipe(gulp.dest('./dist/'));
 });
 
+/**
+ * Demo setup
+ */
 
-gulp.task('surge', ['minify:css', 'minify:js'], function ()
+gulp.task('demo:html', function()
+{
+    return gulp
+        .src('*.html')
+        .pipe(htmlreplace({
+            'ico': {
+                src: './favicon.ico',
+                tpl: '<link rel="icon" type="image/png" href="%s">'
+            },
+            'css': ['./css/autotyped.css','./css/custom.css'],
+            'js': ['./js/autotyped.min.js','./js/custom.min.js']
+        }))
+        .pipe(htmlmin({collapseWhitespace: true, collapseInlineTagWhitespace: true, removeComments: true}))
+        .pipe(gulp.dest('./demo'));
+});
+
+gulp.task('demo:css', function()
+{
+    return gulp
+        .src('./src/scss/**/*.scss')
+        .pipe(sass({errLogToConsole: true, outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(postcss([autoprefixer()]))
+        .pipe(gulp.dest('./demo/css'));
+});
+
+gulp.task('demo:js',  function()
+{
+    return gulp.src('./src/js/autotyped.js')
+        .pipe(concat('autotyped.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./demo/js'));
+});
+
+gulp.task('demo:customjs',  function()
+{
+    return gulp.src('./src/js/custom.js')
+        .pipe(concat('custom.min.js'))
+        .pipe(uglify())
+        .pipe(gulp.dest('./demo/js'));
+});
+
+
+gulp.task('surge', ['demo:html','demo:css', 'demo:js', 'demo:customjs'], function ()
 {
     return surge({
         project: './demo',
@@ -82,5 +142,6 @@ gulp.task('surge', ['minify:css', 'minify:js'], function ()
  * Main tasks
  */
 
-gulp.task('deploy', ['surge']);
+gulp.task('deploy', ['minify:css', 'minify:js']);
+gulp.task('demo', ['surge']);
 gulp.task('default', ['serve']);
